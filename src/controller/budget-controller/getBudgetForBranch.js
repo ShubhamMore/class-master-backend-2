08fd7232357d6e3a41074fdf8875db212db681c6
Shubham More <shubhammore1796@gmail.com>
2020-10-10 14:50:28 +0530

    class-master

--- src/controller/budget-controller/getBudgetForBranch.js
+++ src/controller/budget-controller/getBudgetForBranch.js
@@ -4,23 +4,28 @@ const errorHandler = require('../../handler/error.handler');
 const getBudgetForBranch = async (req, res) => {
   try {
     const query = {
-    
       branch: req.body.branch,
-    }
-    if (req.body.month !=='' && req.body.year!=='') {
+    };
+
+    if (req.body.month !== '' && req.body.year !== '') {
       const date = new RegExp('.*' + req.body.year + '-' + req.body.month + '.*');
-     query.date = date;
-      
-    } else if (req.body.year!=='') {
+      query.date = date;
+    } else if (req.body.year !== '') {
       const date = new RegExp('.*' + req.body.year + '.*');
-    query.date = date;
+      query.date = date;
     }
 
-  
-    const income = await Budget.find({...query, type: 'income'});
-    const expense = await Budget.find({...query, type: 'expense'});
+    const income = Budget.find({ ...query, type: 'income' });
+    const expense = Budget.find({ ...query, type: 'expense' });
 
-    res.status(201).send({ income, expense });
+    Promise.all([income, expense])
+      .then((incomeExpense) => {
+        console.log(incomeExpense);
+        res.status(200).send({ incomes: incomeExpense[0], expenses: incomeExpense[1] });
+      })
+      .catch((e) => {
+        errorHandler(e, 400, res);
+      });
   } catch (e) {
     errorHandler(e, 400, res);
   }
