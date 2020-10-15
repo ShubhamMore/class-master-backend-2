6393bf9bb9aed35e6f05a74651f82f0df134972a
Shubham More <shubhammore1796@gmail.com>
2020-10-16 00:06:16 +0530

    class-master

---
+++ src/controller/employee-salary-controller/getMyBranchSalaries.js
@@ -0,0 +1,37 @@
+const EmployeeSalary = require('../../models/employee-salary.model');
+const errorHandler = require('../../handler/error.handler');
+
+const getMyBranchSalaries = async (req, res) => {
+  try {
+    const searchQuery = {
+      branch: req.body.branch,
+      employee: req.user.imsMasterId,
+    };
+
+    let date = null;
+
+    if (req.body.year !== '' && req.body.month === '') {
+      date = new RegExp('.*' + req.body.year + '.*');
+    }
+
+    if (req.body.month !== '' && req.body.year !== '') {
+      date = new RegExp('.*' + req.body.year + '-' + req.body.month + '.*');
+    }
+
+    if (date) {
+      searchQuery.date = date;
+    }
+
+    const employeeSalary = await EmployeeSalary.find(searchQuery);
+
+    if (!employeeSalary) {
+      throw new Error('Employee Salary Not Found');
+    }
+
+    res.status(200).send(employeeSalary);
+  } catch (e) {
+    errorHandler(e, 400, res);
+  }
+};
+
+module.exports = getMyBranchSalaries;
--- src/routers/employee-salary.route.js
+++ src/routers/employee-salary.route.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth');
 const newEmployeeSalary = require('../controller/employee-salary-controller/newEmployeeSalary');
 const getEmployeeSalaries = require('../controller/employee-salary-controller/getEmployeeSalaries');
 const getBranchEmployeeSalaries = require('../controller/employee-salary-controller/getBranchEmployeeSalaries');
+const getMyBranchSalaries = require('../controller/employee-salary-controller/getMyBranchSalaries');
 const getEmployeeSalary = require('../controller/employee-salary-controller/getEmployeeSalary');
 const changeEmployeeSalaryStatus = require('../controller/employee-salary-controller/changeEmployeeSalaryStatus');
 const deleteEmployeeSalary = require('../controller/employee-salary-controller/deleteEmployeeSalary');
@@ -21,6 +22,10 @@ router.post('/getEmployeeSalaries', auth, async (req, res) => {
 
 router.post('/getBranchEmployeeSalaries', auth, async (req, res) => {
   await getBranchEmployeeSalaries(req, res);
+});
+
+router.post('/getMyBranchSalaries', auth, async (req, res) => {
+  await getMyBranchSalaries(req, res);
 });
 
 router.post('/getEmployeeSalary', auth, async (req, res) => {
