4310c37315d78d4d6fc52e385b671b6a620d87f3
Shubham More <shubhammore1796@gmail.com>
2020-10-26 22:01:12 +0530

    class-master

---
+++ src/controller/institute-controller/getInstituteTransactionHistory.js
@@ -0,0 +1,44 @@
+const PaymentReceipt = require('../../models/payment-receipt.model');
+
+const errorHandler = require('../../handler/error.handler');
+
+const getInstituteBranchHistory = async (req, res) => {
+  try {
+    const branchHistory = PaymentReceipt.aggregate([
+      {
+        $match: {
+          userId: req.body.user,
+        },
+      },
+      {
+        $addFields: {
+          orderId: {
+            $toObjectId: '$orderId',
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'orders',
+          localField: 'orderId',
+          foreignField: '_id',
+          as: 'order',
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$order', 0] }, '$$ROOT'] },
+        },
+      },
+      { $project: { _id: 0, orderId: 0, order: 0 } },
+    ]);
+
+    res.status(200).send(branchHistory);
+
+    res.send(data);
+  } catch (e) {
+    errorHandler(e, 400, res);
+  }
+};
+
+module.exports = getInstituteBranchHistory;
