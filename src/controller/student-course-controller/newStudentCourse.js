a6d11dbe08fa65552b7d97dec3885ec35bfa4212
Shubham More <shubhammore1796@gmail.com>
2020-12-05 23:13:47 +0530

    class-master

--- src/controller/student-course-controller/newStudentCourse.js
+++ src/controller/student-course-controller/newStudentCourse.js
@@ -1,5 +1,9 @@
 const StudentCourse = require('../../models/student-course.model');
 const StudentCourseInstallment = require('../../models/student-course-installment.model');
+
+const mongoose = require('mongoose');
+const sendNotification = require('../../notification/sendNotification');
+
 const errorHandler = require('../../handler/error.handler');
 
 const newStudentCourse = async (req, res) => {
@@ -28,6 +32,109 @@ const newStudentCourse = async (req, res) => {
     await StudentCourse.findByIdAndUpdate(newStudentCourse._id, {
       studentCourseInstallment: newStudentCourseInstallment._id,
     });
+
+    const studentCourseDetails = await StudentCourse.aggregate([
+      {
+        $match: {
+          _id: mongoose.Types.ObjectId(newStudentCourse._id),
+        },
+      },
+      {
+        $addFields: {
+          branchId: {
+            $toObjectId: '$branch',
+          },
+          categoryId: {
+            $toObjectId: '$category',
+          },
+          courseId: {
+            $toObjectId: '$course',
+          },
+          batchId: {
+            $toObjectId: '$batch',
+          },
+          installmentId: {
+            $toObjectId: '$studentCourseInstallment',
+          },
+        },
+      },
+      {
+        $lookup: { from: 'branches', localField: 'branchId', foreignField: '_id', as: 'branches' },
+      },
+      { $lookup: { from: 'courses', localField: 'courseId', foreignField: '_id', as: 'courses' } },
+      { $lookup: { from: 'batches', localField: 'batchId', foreignField: '_id', as: 'batches' } },
+      {
+        $lookup: {
+          from: 'studentcourseinstallments',
+          localField: 'installmentId',
+          foreignField: '_id',
+          as: 'installments',
+        },
+      },
+      {
+        $lookup: {
+          from: 'students',
+          localField: 'student',
+          foreignField: 'imsMasterId',
+          as: 'students',
+        },
+      },
+      {
+        $addFields: {
+          branch: { $arrayElemAt: ['$branches', 0] },
+          course: { $arrayElemAt: ['$courses', 0] },
+          batch: { $arrayElemAt: ['$batches', 0] },
+          installments: { $arrayElemAt: ['$installments', 0] },
+          student: { $arrayElemAt: ['$students', 0] },
+        },
+      },
+      {
+        $addFields: {
+          category: {
+            $filter: {
+              input: '$branch.categories',
+              as: 'curCategory',
+              cond: {
+                $eq: ['$$curCategory._id', '$categoryId'],
+              },
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          branches: 0,
+          'branch.categories': 0,
+          'branch.currentPlanDetails': 0,
+          courses: 0,
+          batches: 0,
+          students: 0,
+          branchId: 0,
+          categoryId: 0,
+          courseId: 0,
+          batchId: 0,
+          installmentId: 0,
+        },
+      },
+    ]);
+
+    const notificationMessage = `Hii ${
+      studentCourseDetails.student.name
+    }, you are successfully registered to ${
+      studentCourseDetails.batch.basicDetails.batchName
+    } batch for ${
+      studentCourseDetails.course.basicDetails.courseName
+    } course starting from ${studentCourseDetails.batch.basicDetails.startDate
+      .split('-')
+      .reverse()
+      .join('-')} with Roll Number ${studentCourseDetails.rollNumber} in Institute ${
+      studentCourseDetails.branch.basicDetails.branchName
+    } (${studentCourseDetails.category.category}) - ${studentCourseDetails.branch.address.city}.`;
+
+    const notificationTitle = 'New Course Registered';
+    const receiverId = studentCourseDetails.student.imsMAsterId;
+
+    await sendNotification(notificationTitle, notificationMessage, receiverId);
 
     res.status(201).send({ success: true });
   } catch (e) {
